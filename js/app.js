d5d5de83e759bae454673746a99072f4ef24847f
ChrisBarrio <ch.barrionuevo89@gmail.com>
2022-04-22 10:51:41 -0300

    commit 'se agrega sonido y modifican botones'

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
-const btn_encendido = document.querySelector('.btn_on');
+const btn_on = document.querySelector('.btn_on');
+const btn_off = document.querySelector('.btn_off');
 const led_on = document.querySelector('.battery_point');
 const animation_nintendo = document.querySelector('.animated-text');
+const green_screen = document.querySelector('.green_screen');
+const audio = document.querySelector('audio');
 
-btn_encendido.addEventListener ('click', () => {
-    led_on.classList.toggle('power-on')
-    animation_nintendo.classList.toggle('end')
+// Play Sound
+function playSound () {
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+btn_on.addEventListener ('click', () => {
+    led_on.classList.add('power-on');
+    animation_nintendo.classList.add('end');
+    green_screen.classList.add('active');
+
+    if(animation_nintendo){
+        const myTimeout = setTimeout(playSound, 3000);
+    }
+});
+
+btn_off.addEventListener ('click', () => {
+    led_on.classList.remove('power-on');
+    animation_nintendo.classList.remove('end');
+    green_screen.classList.remove('active');
+    
 })
+
